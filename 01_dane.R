988c947153c8db2da7589290dfb05dea9b5cb34c
lwawrowski <lukasz8989@gmail.com>
2020-01-18 14:24:20 +0100

    dane i wizualizacja

---
+++ 01_dane.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(readxl)
+library(lubridate)
+# http://powietrze.gios.gov.pl/pjp/archives
+
+pm10_2015 <- read_xlsx("data/pm10_pszczyna.xlsx", sheet = "2015")
+pm10_2016 <- read_xlsx("data/pm10_pszczyna.xlsx", sheet = "2016")
+pm10_2017 <- read_xlsx("data/pm10_pszczyna.xlsx", sheet = "2017")
+pm10_2018 <- read_xlsx("data/pm10_pszczyna.xlsx", sheet = "2018")
+
+pm10_pszczyna <- union_all(pm10_2015, pm10_2016) %>% 
+  union_all(., pm10_2017) %>% 
+  union_all(., pm10_2018) %>% 
+  mutate(data=as_date(data),
+         dzien_tyg=factor(wday(data),
+                          levels = 1:7, 
+                          labels = c("Pn", "Wt", "Śr", "Czw", "Pt", "Sb", "Nd"),
+                          ordered = T),
+         kwartal=str_c("Kwartał ", quarter(data)),
+         rok=as.factor(year(data)),
+         przekroczenie=pm10>50)
+
+library(imgw)
+
+d <- meteo_daily(rank = "climate", year = 2015:2018)
+
+imgw_pszczyna <- d %>% 
+  filter(station == "PSZCZYNA") %>% 
+  mutate(data=as_date(str_c(yy,"-",mm,"-",day)))
---
+++ 02_wizualizacja.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+
+# gioś
+ggplot(pm10_pszczyna, aes(x=data, y = pm10, color = przekroczenie)) +
+  geom_point() +
+  geom_smooth()
+
+pm10_pszczyna %>% 
+  group_by(rok, dzien_tyg) %>% 
+  summarise(srednia = mean(pm10, na.rm=T)) %>% 
+  ggplot(aes(x=dzien_tyg, y=srednia, fill=rok)) +
+  geom_col(position="dodge")
+
+pm10_pszczyna %>% 
+  group_by(rok, kwartal) %>% 
+  summarise(srednia = mean(pm10, na.rm=T)) %>% 
+  ggplot(aes(x=kwartal, y=srednia, fill=rok)) +
+  geom_col(position="dodge")
+
+ggplot(pm10_pszczyna, aes(x=dzien_tyg, y=pm10, fill=rok)) +
+  geom_boxplot()
+
+ggplot(pm10_pszczyna, aes(x=kwartal, y=pm10, fill=rok)) +
+  geom_boxplot()
+
+# imgw
+ggplot(imgw_pszczyna, aes(x=data, y=t2m_mean_daily)) +
+  geom_point() +
+  geom_smooth()
+
+pm10_imgw <- select(imgw_pszczyna, data, t2m_mean_daily) %>% 
+  inner_join(., select(pm10_pszczyna, data, pm10)) %>% 
+  pivot_longer(-data)
+
+ggplot(pm10_imgw, aes(x=data, y=value, color=name)) +
+  geom_point() +
+  geom_smooth()
+
+pm10_imgw_std <- select(imgw_pszczyna, data, t2m_mean_daily) %>% 
+  inner_join(., select(pm10_pszczyna, data, pm10)) %>% 
+  mutate_if(is.numeric, scale) %>% 
+  pivot_longer(-data)
+
+ggplot(pm10_imgw_std, aes(x=data, y=value, color=name)) +
+  geom_point() +
+  geom_smooth()
+

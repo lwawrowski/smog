a9aaa351ceec6ed5a1f0e4729866084c83d5bb1c
lwawrowski <lukasz8989@gmail.com>
2020-02-17 23:24:52 +0100

    wizualizacja pm10 wiatr

--- 04_wiatr.R
+++ 04_wiatr.R
@@ -25,8 +25,6 @@ wiatr_kierunek <- wiatr_kierunek %>%
   mutate(data=as_date(data_time),
          kierunek8=cut(x=kierunek, breaks = seq(from = 0, to = 360, by = 22.5), 
                        labels = c("N", "NE", "NE", "E", "E", "SE", "SE", "S", "S", "SW", "SW", "W", "W", "NW", "NW", "N")))
-  
-save(wiatr_kierunek, file="data/wiatr_kierunek.RData")
 
 # siła wiatru
 
@@ -38,6 +36,8 @@ wiatr_sila <- wiatr_sila %>%
   select(data_time=X3, sila=X4)
 
 wiatr_kierunek_sila <- inner_join(wiatr_kierunek, wiatr_sila, by = "data_time")
+
+save(wiatr_kierunek_sila, file="data/wiatr_kierunek_sila.RData")
 
 # dominanta dzienna
 
@@ -71,8 +71,33 @@ ggplot(wiatr_kierunek_dzien_dom, aes(x=data, y=sila_med, color=kierunek8)) +
 
 load("data/pm10.RData")
 
-pm10_wiatr <- inner_join(pm10_pszczyna, wiatr_kierunek_dzien_dom, by="data")
+pm10_wiatr <- inner_join(pm10_pszczyna, wiatr_kierunek_dzien_dom, by="data") 
+
+ggplot(pm10_wiatr, aes(x=rok, y=sila_med, fill=kwartal)) + geom_boxplot()
+
+kwartal_wiatr <- pm10_wiatr %>% 
+  count(kwartal, kierunek8)
+
+treemap::treemap(kwartal_wiatr, index = c("kwartal", "kierunek8"), vSize = "n")
 
 table(pm10_wiatr$poziom, pm10_wiatr$kierunek8)
 
 table(pm10_wiatr$indeks, pm10_wiatr$kierunek8)
+
+ggplot(pm10_wiatr, aes(y=pm10, x=sila_med, color=kierunek8)) +
+  geom_point()
+
+indeks_wiatr <- pm10_wiatr %>% 
+  count(indeks, kierunek8)
+
+treemap::treemap(indeks_wiatr, index = c("indeks", "kierunek8"), vSize = "n")
+
+poziom_wiatr <- pm10_wiatr %>% 
+  count(poziom, kierunek8)
+
+treemap::treemap(poziom_wiatr, index = c("poziom", "kierunek8"), vSize = "n")
+
+pm10_wiatr %>% 
+  count(indeks, kierunek8) %>% 
+  group_by(indeks) %>% 
+  mutate(proc=n/n())

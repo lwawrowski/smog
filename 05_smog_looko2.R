54aaa6e2282f8d8eb2d81a9c7190bfd095fcee86
lwawrowski <lukasz8989@gmail.com>
2020-03-18 16:19:04 +0100

    pobieranie danych z looko2

--- 05_smog_looko2.R
+++ 05_smog_looko2.R
@@ -7,3 +7,20 @@ d <- read_csv("looko2/Archives_2019-01-01.csv", col_names = F) %>%
 
 d <- read_csv("looko2/Archives_2017-01-01.csv", col_names = F) %>% 
   filter(X10 == "PSZCZYNA_Bielska10")
+
+try(download.file(url = "http://looko2.com/Archives/2017/Archives_2017-01-02.csv", destfile = "looko2/Archives_2017-01-02.csv"))
+
+pobierz_looko2 <- function(){
+  
+  dni <- seq(as.Date("2017-01-01"), as.Date("2017-12-31"), by="+1 day")
+  
+  for(dzien in 1:length(dni)){
+    
+    try(download.file(url = paste0("http://looko2.com/Archives/2017/Archives_",dni[dzien],".csv"), destfile = paste0("looko2/d_",dni[dzien],".csv")))
+    Sys.sleep(1)
+    
+  }
+  
+}
+
+pobierz_looko2()

80281ba50783c7a8fc6c509cff3fa9ffe3079aac
lwawrowski <lukasz8989@gmail.com>
2020-03-19 14:46:18 +0100

    pobranie wszystkich danych z looko2

--- 05_smog_looko2.R
+++ 05_smog_looko2.R
@@ -12,11 +12,11 @@ try(download.file(url = "http://looko2.com/Archives/2017/Archives_2017-01-02.csv
 
 pobierz_looko2 <- function(){
   
-  dni <- seq(as.Date("2017-01-01"), as.Date("2017-12-31"), by="+1 day")
+  dni <- seq(as.Date("2019-01-01"), as.Date("2019-12-31"), by="+1 day")
   
   for(dzien in 1:length(dni)){
     
-    try(download.file(url = paste0("http://looko2.com/Archives/2017/Archives_",dni[dzien],".csv"), destfile = paste0("looko2/d_",dni[dzien],".csv")))
+    try(download.file(url = paste0("http://looko2.com/Archives/Archives_",dni[dzien],".csv"), destfile = paste0("looko2/d_",dni[dzien],".csv")))
     Sys.sleep(1)
     
   }
